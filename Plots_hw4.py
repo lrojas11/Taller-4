1e0aef8cc119bbae771692f16486606a5e86594e
Laura Rojas <42391116+lrojas11@users.noreply.github.com>
2018-11-19 10:40:33 -0500

    Falta graficar punto 3

--- Plots_hw4.py
+++ Plots_hw4.py
@@ -23,3 +23,24 @@ plt.ylabel('Distancia Vertical[Mts]')
 plt.legend(('10 $^\circ$','20 $^\circ$','30 $^\circ$','40$^\circ$','50$^\circ$','60$^\circ$','70$^\circ$','45$^\circ$'),loc='upper right')
 plt.savefig('dinamica_proyectil.png')
 plt.close()
+
+#PUNTO 3 ECUACION DE DIFUSION
+
+
+#Como la malla no es cuadrada, se importa axes3d:
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+#Leer los datos para los tres casos
+punto1=np.loadtxt('punto1.txt',float,delimiter=',',usecols=range(36))
+punto2=np.loadtxt('punto2.txt',float,delimiter=',',usecols=range(36))
+punto3=np.loadtxt('punto3.txt',float,delimiter=',',usecols=range(36))
+
+#Cargar las coordenadas de los puntos:
+nodos=np.loadtxt('coordenadas.txt',float,delimiter=',')
+
+#Vectores X y Y con las coordenadas de los puntos importados en nodos:
+X=nodos[:,0]
+Y=nodos[:,1]
+T=punto1[0,:]
+
